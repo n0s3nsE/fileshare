e5477656bb9b4862ab2606b72fc1da0e967050e8
N0s3nsE <N0s3nsE.NNS@gmail.com>
2021-12-10 13:29:44 +0900

    add checkbox func

--- src/components/ContentsComponent.vue
+++ src/components/ContentsComponent.vue
@@ -8,7 +8,7 @@
         <thead>
           <tr>
             <th>
-              <input type="checkbox" @change="all_check()" />
+              <input type="checkbox" @change="select_all()" v-model="selected_all" />
             </th>
             <th>ファイル名</th>
             <th>更新</th>
@@ -18,7 +18,7 @@
         <tbody>
           <tr v-for="(item, index) in items" :key="index">
             <td>
-              <input type="checkbox" @change="change_check(index)" />
+              <input type="checkbox" @change="select_change()" :value="item.id" v-model="selected_items" />
             </td>
             <td v-if="item.isfolder">
               <a :href="current_path + item.name">
@@ -48,41 +48,58 @@ export default {
       items: [],
       uploading_items: [],
       current_path: '',
+      selected_items: [],
+      selected_all: false,
     };
   },
   mixins: [Mixin],
   mounted() {
     //axios
-    //test function
-    this.items = [
+    //test data
+    const receive_data = [
       {
+        id: 1, //require, PK
         name: 'test_item1', //require
         updatedAt: '2021-12-08', //require
-        isfolder: false, //require
         size: 1,
+        isfolder: false, //require
         islocked: false, //require
       },
       {
-        name: 'test_item2', //require
-        updatedAt: '2021-12-08', //require
-        isfolder: false, //require
+        id: 2,
+        name: 'test_item2', 
+        updatedAt: '2021-12-08',
         size: 1,
-        islocked: true, //require
+        isfolder: false,
+        islocked: true,
       },
       {
-        name: 'test_folder', //require
-        updatedAt: '2021-12-08', //require
-        isfolder: true, //require
-        size: 1,
-        islocked: false, //require
+        id: 3,
+        name: 'test_folder',
+        updatedAt: '2021-12-08',
+        size: null,
+        isfolder: true,
+        islocked: false,
       },
     ];
-    this.current_path = this.get_path() + '/';
+    this.items = receive_data;
+
+    this.current_path = this.get_path();
   },
   methods: {
-    change_check(index) {
-      this.items[index].ischecked = !this.items[index].ischecked;
+    select_all() {
+      if(this.selected_all) {
+        this.selected_items = this.items.map((a) => a.id);
+      }
+      else {
+        this.selected_items = [];
+      }
     },
+    select_change() {
+      if(this.selected_all) {
+        this.selected_all = false;
+      }
+    }
   },
 };
 </script>
