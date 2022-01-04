629d5a7861597bbc3493b0f076ac4d4fbc310888
N0s3nsE <N0s3nsE.NNS@gmail.com>
2022-01-05 01:00:13 +0900

    wip

---
+++ frontend/src/components/tools/LockbuttonComponent.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <button @click="send_locksignal">svg lockicon{{ item_id }}</button>
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+import Mixin from "../../mixin/mixin";
+
+export default {
+  props: ["item_id"],
+  data() {
+    return {
+      lock_api_url: "http://127.0.0.1:8000/api/lock",
+    };
+  },
+  mixins: Mixin,
+  methods: {
+    send_locksignal() {
+      axios
+        .get(this.lock_api_url + "/" + this.item_id)
+        .then(() => this.get_itemlist(this.current_path))
+        .catch((error) => {});
+    },
+  },
+};
+</script>
